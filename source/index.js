d447ee90a2b79d86bde7d7c1ff7fc3b0c91030de
Yu He <yuhe00@gmail.com>
2016-12-19 09:52:10 +0100

    Change entrypoint from fullscreen to embedded div

--- source/index.js
+++ source/index.js
@@ -3,7 +3,7 @@ const Elm = require('./Main');
 require('elm-ui/stylesheets/main.scss');
 require('./stylesheets/main.scss');
 
-var app = Elm.Main.fullscreen({
+var app = Elm.Main.embed(document.getElementById('app'), {
   apiToken: window.apiToken,
   apiHost: window.config && window.config.apiUri
 });
